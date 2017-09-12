86cc56de907087f9e7ec537f72a97e2b60142138
4chazz <chazzbrooks@gmail.com>
2017-09-12 17:41:53 +0000

    Database Migrated

--- db/migrate/20170912173756_create_contacts.rb
+++ db/migrate/20170912173756_create_contacts.rb
@@ -1,10 +1,10 @@
 class CreateContacts < ActiveRecord::Migration[5.0]
   def change
     create_table :contacts do |t|
-      t.text    :name
-      t.string  :email
-      t.string  :phone
-      t.text    :message
+      t.text :name
+      t.string :email
+      t.string :phone
+      t.text :message
 
       t.timestamps
     end
