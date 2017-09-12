e0ac928e2537366ff91ffcaf2fd9c0c123353197
4chazz <chazzbrooks@gmail.com>
2017-09-12 22:57:39 +0000

    Edit Controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,11 @@
 class ContactsController < ApplicationController
 
-<<<<<<< HEAD
-
-=======
-# add this to master
->>>>>>> 69082a708431d6a73ac28f526cbe02d3d099acab
   def new
     @contact = Contact.new
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
   end
 
 
